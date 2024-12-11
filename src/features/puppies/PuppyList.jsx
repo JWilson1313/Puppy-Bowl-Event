858b46ba0daf229858fbb0ebdfe9207b5d63810f
JWiilson1313 <jdbw13@hotmail.com>
2024-12-11 09:55:22 -0700

    Added new feature

--- src/features/puppies/PuppyList.jsx
+++ src/features/puppies/PuppyList.jsx
@@ -1,3 +1,5 @@
+import { useGetPuppiesQuery } from "./puppySlice";
+
 /**
  * @component
  * Shows a list of puppies in the roster.
@@ -5,6 +7,15 @@
  */
 export default function PuppyList({ setSelectedPuppyId }) {
   // TODO: Get data from getPuppies query
+  const { data: puppies, isLoading, error } = useGetPuppiesQuery();
+
+  if (isLoading) {
+    return <p>Loading.....</p>;
+  }
+
+  if (error) {
+    return <p>There was an error: {error.message}</p>;
+  }
 
   return (
     <article>
--- src/features/puppies/puppySlice.js
+++ src/features/puppies/puppySlice.js
@@ -15,9 +15,41 @@ functions for each endpoint.
 */
 
 const puppyApi = api.injectEndpoints({
-  endpoints: (build) => ({}),
-});
+  endpoints: (build) => ({
+    getPuppies: build.query({
+      query: () => '/players',
+      providedTags: ["Puppy"],
+      transformResponse: (response) => response.data.players,
+      transformErrorResponse: (error) => error,
+      }),
+      getPuppy: build.query({
+        query: (id) => `/players/${id}`,
+        providesTags: ["Puppy"],
+        transformResponse: (response) => response.data,
+        transformErrorResponse: (error) => error,
+      }),
+        addPuppy: build.mutation({
+          query: (newPuppy) => ({
 
+            url: "/players",
+            method: "POST",
+            body: newPuppy,
+      }),
+      invalidatesTags: ["Puppy"],
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (error) => error,
+    }), 
+    deletePuppy: build.mutation({
+      query: (id) => ({
+        url:  `/players/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Puppy"],
+      transformResponse: (response) => response.data,
+    transformErrorResponse: (error) => error,   
+  }),
+  }), 
+});
 export const {
   useGetPuppiesQuery,
   useGetPuppyQuery,
